bd25f453980eb27bfefff1e30ad072b141b09423
Stanly0407 <Shelestova_S_V@mail.ru>
2020-11-04 01:11:40 +0300

    Callable Future Ex

--- src/main/java/muConcurrentEx/executors/CallableFutureEx.java
+++ src/main/java/muConcurrentEx/executors/CallableFutureEx.java
@@ -6,10 +6,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.*;
 
+// Основная идея данного примера – показать, как можно, используя Future, узнать статус Callable потока и получить возвращенный объект.
+// В примере используется объект executor типа ExecutorService, формирующий пул из трех потоков.
+// Метод submit с параметром Callable возвращает объект Future для каждого из стартуемого потоков.
+
 public class CallableFutureEx {
 
     public CallableFutureEx() {
-
         // Определяем пул из трех потоков
         ExecutorService executor = Executors.newFixedThreadPool(3);
 
@@ -20,30 +23,17 @@ public class CallableFutureEx {
         Callable<String> callable = new CallableClass();
 
         for (int i = 0; i < 3; i++) {
-            /*
-             * Стартуем возвращаюший результат исполнения
-             * в виде объекта Future поток
-             */
-            Future<String> future;
-            future = executor.submit(callable);
-            /*
-             * Добавляем объект Future в список для
-             * отображения результат выполнения (получение
-             * наименования потока)
-             */
+            // Стартуем, возвращаюший результат исполнения в виде объекта Future поток
+            Future<String> future = executor.submit(callable);
+            // Добавляем объект Future в список для отображения результат выполнения (получение наименования потока)
             futures.add(future);
         }
-        SimpleDateFormat sdf;
-        sdf = new SimpleDateFormat("HH:mm:ss  ");
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss  ");
         for (Future<String> future : futures) {
-            try {
-                // Выводим в консоль полученное значение
-                String text = sdf.format(new Date())
-                        + future.get();
+            try {  // Выводим в консоль полученное значение
+                String text = sdf.format(new Date()) + future.get();
                 System.out.println(text);
-            } catch (InterruptedException |
-                    ExecutionException e) {
-            }
+            } catch (InterruptedException | ExecutionException e) {}
         }
         // Останавливаем пул потоков
         executor.shutdown();
@@ -55,8 +45,7 @@ public class CallableFutureEx {
         @Override
         public String call() throws Exception {
             Thread.sleep(1000);
-            // наименование потока, выполняющего
-            // callable задачу
+            // наименование потока, выполняющего callable задачу
             return Thread.currentThread().getName();
         }
     }
