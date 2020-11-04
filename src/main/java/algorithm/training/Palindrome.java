9df30ef94cfe35061ff02dea5643dcdacac9e386
Stanly0407 <Shelestova_S_V@mail.ru>
2020-11-05 00:52:10 +0300

    isPalindrome

---
+++ src/main/java/algorithm/training/Palindrome.java
@@ -0,0 +1,42 @@
+package algorithm.training;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String args[]){
+
+        System.out.println("Please Enter a number : ");
+        int palindrome = new Scanner(System.in).nextInt();
+
+        if(isPalindrome(palindrome)){
+            System.out.println("Number : " + palindrome + " is a palindrome");
+        }else{
+            System.out.println("Number : " + palindrome + " is not a palindrome");
+        }
+
+    }
+
+    /*
+     * Java method to check if a number is palindrome or not
+     */
+    public static boolean isPalindrome(int number) {
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+//если исходное и обратное числа равны, значит, число является палиндромом в Java
+        if (number == reverse) {
+            return true;
+        }
+        return false;
+    }
+
+}
+
+
+
