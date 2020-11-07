04a6973bf672ec64999f2d1f54a18cd6704007ed
Stanly0407 <Shelestova_S_V@mail.ru>
2020-11-07 15:22:55 +0300

    Factorial ex (Recursion)

--- src/main/java/algorithm/training/FactorialDemo1.java
+++ src/main/java/algorithm/training/FactorialDemo1.java
@@ -3,7 +3,7 @@ package algorithm.training;
 import java.util.Scanner;
 
 public class FactorialDemo1 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number:");
@@ -11,17 +11,17 @@ public class FactorialDemo1 {
         int num = scanner.nextInt();
 
         int factorial = fact(num);
-        System.out.println("Factorial of entered number is: "+factorial);
+        System.out.println("Factorial of entered number is: " + factorial);
 
     }
-    static int fact(int n)
-    {
+
+    static int fact(int n) {
         int output;
-        if(n==1 || n==0){
+        if (n == 1 || n == 0) {
             return 1;
         }
 
-        output = fact(n-1)* n;
+        output = fact(n - 1) * n;
         return output;
     }
 }
