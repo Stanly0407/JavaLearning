b3b8cd615d9c86d299a26feb21aaac25078a3cd8
Stanly0407 <Shelestova_S_V@mail.ru>
2020-11-07 18:46:50 +0300

    is Palindrome String

--- src/main/java/algorithm/training/BinaryToDecimal.java
+++ src/main/java/algorithm/training/BinaryToDecimal.java
@@ -11,30 +11,21 @@ public class BinaryToDecimal {
             if(binaryNumber == 0){
                 break;
             } else {
-                int temp = binaryNumber%10;
-                decimal += temp*Math.pow(2, p);
-                binaryNumber = binaryNumber/10;
+                int temp = binaryNumber % 10;
+                decimal += temp * Math.pow(2, p);
+                binaryNumber = binaryNumber / 10;
                 p++;
             }
         }
         return decimal;
     }
     public static void main(String args[]){
+
         Scanner input = new Scanner( System.in );
         System.out.print("Enter a binary number: ");
         int binar = input.nextInt();
 
         System.out.println("Output: " + binaryToDecimal(binar));
+
     }
-
-
-
-
-
-//        Details obj = new Details();
-//        System.out.println("110 --> "+obj.BinaryToDecimal(110));
-//        System.out.println("1101 --> "+obj.BinaryToDecimal(1101));
-//        System.out.println("100 --> "+obj.BinaryToDecimal(100));
-//        System.out.println("110111 --> "+obj.BinaryToDecimal(110111));
-
 }
---
+++ src/main/java/algorithm/training/GCDTwoNumbers.java
@@ -0,0 +1,5 @@
+package algorithm.training;
+
+public class GCDTwoNumbers {
+    // Наибольший ощий делитель двух чисел (НОД = GCD (Greatest Common Divisor))
+}
