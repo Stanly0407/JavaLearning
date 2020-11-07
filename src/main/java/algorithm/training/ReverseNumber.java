e56c9b86f2293cc7f2584d1dd257a5f55081fcea
Stanly0407 <Shelestova_S_V@mail.ru>
2020-11-07 15:26:56 +0300

    Reverse Number ex

--- src/main/java/algorithm/training/ReverseNumber.java
+++ src/main/java/algorithm/training/ReverseNumber.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class ReverseNumber {
 
     public static void main(String args[]) {
-        int num = 0;
+        int num;
         int reversenum = 0;
         System.out.println("Input your number and press enter: ");
-        //This statement will capture the user input
+
         Scanner in = new Scanner(System.in);
-        //Captured input would be stored in number num
+
         num = in.nextInt();
-        //While Loop: Logic to find out the reverse number
+
         while (num != 0) {
             reversenum = reversenum * 10;
             reversenum = reversenum + num % 10;
