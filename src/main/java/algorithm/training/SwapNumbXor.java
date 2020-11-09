9e23778db552983a6ff16a02725393b185711b36
Stanly0407 <Shelestova_S_V@mail.ru>
2020-11-09 15:11:34 +0300

    Swap Numb Xor

---
+++ src/main/java/algorithm/training/SwapNumbXor.java
@@ -0,0 +1,36 @@
+package algorithm.training;
+
+
+//побитовый оператор XOR: сравнивает соответствующие биты двух операндов и возвращает 1, если они равны,
+// и 0, если они не равны. Например:
+//num1 = 11; /* equal to 00001011*/
+//num2 = 22; /* equal to 00010110 */
+
+import java.util.Scanner;
+
+public class SwapNumbXor {
+    public static void main(String args[]) {
+        int num1, num2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        num1 = scanner.nextInt();
+        System.out.print("Enter second number:");
+        num2 = scanner.nextInt();
+        /* To make you understand, lets assume I am going
+         * to enter value of first number as 10 and second
+         * as 5. Binary equivalent of 10 is 1010 and 5 is
+         * 0101
+         */
+
+        //num1 becomes 1111 = 15
+        num1 = num1 ^ num2;
+        //num2 becomes 1010 = 10
+        num2 = num1 ^ num2;
+        //num1 becomes 0101 = 5
+        num1 = num1 ^ num2;
+        scanner.close();
+        System.out.println("The First number after swapping:" + num1);
+        System.out.println("The Second number after swapping:" + num2);
+    }
+
+}
