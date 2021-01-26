ae8b3772c4cd46a61698e4a6270ecb923469952b
Stanly0407 <Shelestova_S_V@mail.ru>
2021-01-26 14:40:39 +0300

    selection sort algorithm

--- src/main/java/algorithm/sorting/SelectionSort.java
+++ src/main/java/algorithm/sorting/SelectionSort.java
@@ -1,5 +1,11 @@
 package algorithm.sorting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.swap;
+
 public class SelectionSort {
 
     // имеет квадратичную сложность
@@ -8,7 +14,29 @@ public class SelectionSort {
     // При этом каждый новый проход начинать сдвигаясь вправо,
     // то есть первый проход — с первого элемента, второй проход — со второго.
     // Выглядеть это будет следующим образом:
+    public static void main(String[] args) {
 
+        List<Integer> elements = new ArrayList<>();
+        elements.add(10);
+        elements.add(-2);
+        elements.add(55);
+        elements.add(-52);
+        elements.add(102);
+        elements.add(0);
 
-
+        for (int min = 0; min < elements.size() - 1; min++) {
+            int minElement = min;
+            for (int i = min + 1; i < elements.size(); i++) {
+                if (elements.get(i) < elements.get(minElement)) {
+                    minElement = i;
+                }
+            }
+            int temporaryElement = elements.get(min);
+            elements.set(min, elements.get(minElement));
+            elements.set(minElement, temporaryElement);
+        }
+        for (Integer element : elements){
+            System.out.println(element);
+        }
+    }
 }
